8c77a39b3b419b3a944b2962d129bf261cdc3178
EddieUFSM <edrocha@inf.ufsm.br>
2021-05-09 21:00:25 -0300

    001 persist events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,13 +5,21 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 
 app.post('/events', async (req, res) => {
     const event = req.body
+
+    event.push(event)
     axios.post('http://localhost:4000/events', event)
     axios.post('http://localhost:4001/events', event)
     axios.post('http://localhost:4002/events', event)
     axios.post('http://localhost:4003/events', event)
+})
+
+app.get('/events', (req, res) => {
+    res.send(events)
 })
 
 app.listen(4005, () => {
